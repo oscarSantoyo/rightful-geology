1d4477250aadd7d949423c2e2d92fc21db90b859
Oscar Santoyo <oscar.santoyo@wizeline.com>
2018-11-17 17:45:07 -0600

    First version

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').bxSlider();
+  });
+  $(document).ready(function(){
+$('.bxslider').bxSlider({
+  slideWidth: 900,
+  minSlides: 2,
+  maxSlides: 3,
+  moveSlides: 1,
+  pager: false,
+  auto: false
+});
+});
